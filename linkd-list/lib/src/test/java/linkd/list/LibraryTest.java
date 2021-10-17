db0e76b71813031e226330e3ef0f947d33fa3988
abdallah hamoury <85105703+abdallahhamoury@users.noreply.github.com>
2021-10-17 21:24:49 +0300

    Update LibraryTest.java

--- linkd-list/lib/src/test/java/linkd/list/LibraryTest.java
+++ linkd-list/lib/src/test/java/linkd/list/LibraryTest.java
@@ -6,8 +6,56 @@ package linkd.list;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class LibraryTest {
+class LibraryTest { 
      
+     @Test Student void listStringTest()
+     {  
+          
+          LinkedList test = new LinkedList();  
+          
+          
+          assertNotNull(test.toString(), "{NULL}");    }
+     
+    @Test Student void listStringTest2()
+         
+         
+    {     
+        
+         
+         LinkedList test2 = new LinkedList();  
+         
+         
+     test2.insert(1);  
+     test2.insert(2);  
+     test2.insert(3);  
+     test2.insert(4);  
+     test2.insert(5);  
+         
+     assertEquals
+          
+          
+   (test2.listString(),"{ 5 } -> { 4 } -> { 3 } -> { 2 } -> { 1 } -> NULL");
+    
     }
+     
+    @Test Student void listStringTest3(){  
+         
+         
+         
+         LinkedList test2 = new LinkedList(); 
+         test2.insert(1); 
+         test2.insert(2);    
+         test2.insert(3);    
+         test2.insert(4);   
+         test2.insert(5);    
+         test2.append(6);    
+         test2.insertBefore(6,0);    
+         test2.insertBefore(6,1);    
+         test2.insertAfter(3,11);    
+         assertEquals(test2.listString(),"{ 5 } -> { 4 } -> { 3 } -> { 11 } -> { 2 } -> { 1 } -> { 0 } -> { 1 } -> { 6 } -> NULL"); 
+    
+    }
+     
+
   
 
