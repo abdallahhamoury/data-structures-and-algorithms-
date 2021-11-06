ffb399d197c30f1e1f731ddcad206e84f2586d6e
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-11-06 22:18:57 +0200

    update

--- trees/app/src/main/java/trees/App.java
+++ trees/app/src/main/java/trees/App.java
@@ -4,6 +4,7 @@
 package trees;
 
 public class App {
+
     public String getGreeting() {
         return "Hello World!";
     }
@@ -45,5 +46,27 @@ public class App {
         newTree.addFuction(4);
         newTree.addFuction(5);
         System.out.println(newTree.traversalFunction(newTree));
+
+        KTree ktree = new KTree();
+        NodeK root0 = new NodeK("24");
+        NodeK node10 = new NodeK("15");
+        NodeK node20 = new NodeK("20");
+        NodeK node30 = new NodeK("65");
+        NodeK node40 = new NodeK("30");
+        NodeK node50 = new NodeK("555");
+        NodeK node60 = new NodeK("18");
+
+        ktree.root = root0;
+        root0.children.add(node10);
+        root0.children.add(node20);
+        node10.children.add(node30);
+        node20.children.add(node40);
+        node20.children.add(node50);
+        node20.children.add(node60);
+
+        System.out.println("fizzBuzz");
+        System.out.println(ktree.fizzBuzz(root0));
+
+
     }
 }
