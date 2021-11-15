5d48d40e485ef3df643cff093cb6a250137bc4a5
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-11-15 16:06:18 +0200

    update

--- insertsort/app/src/test/java/insertsort/AppTest.java
+++ insertsort/app/src/test/java/insertsort/AppTest.java
@@ -12,15 +12,26 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test  void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
-    @Test void testsort(){
+    @Test  void testsort(){
         Sort sort=new Sort();
         int [] arr={8,4,23,42,16,15};
         int [] res={4,8,15,16,23,42};
+//        assertEquals(res, Arrays.toString(sort.insertionSort(arr)));
         assertEquals(Arrays.toString(res), Arrays.toString(sort.insertionSort(arr)));
+    }
+    @Test  void testsortmerge(){
+        MergeSort mergeSort=new MergeSort();
+        int [] arr={8,4,23,42,16,15};
+        int [] res={4,8,15,16,23,42};
+//        assertEquals(res, Arrays.toString(sort.insertionSort(arr)));
+        assertEquals(Arrays.toString(res), Arrays.toString(mergeSort.mergeSort(arr)));
     }
 }
